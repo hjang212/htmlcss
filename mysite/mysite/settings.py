bd6df1cca3a304bf8c08f7a1613eed66dbfa4821
hjang212 <hjjang8812@gmail.com>
2022-08-23 16:31:24 +0900

    fourth commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.postgresql',
     }
 }
 
